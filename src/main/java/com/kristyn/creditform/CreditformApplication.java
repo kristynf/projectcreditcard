ae6a32666413d4885a5f35338cfeccfcd826467e
kristynf <kristynf@bu.edu>
2020-03-06 13:50:33 -0500

    initial commit

---
+++ src/main/java/com/kristyn/creditform/CreditformApplication.java
@@ -0,0 +1,13 @@
+package com.kristyn.creditform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kristyn/creditform/CreditformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kristyn.creditform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
